a891ddf78e095ad9b8005788655daf9eff4b4160
zhuzhui <zhuzhui0359@outlook.com>
2025-02-28 19:21:33 +0800

    debug

--- src/CircCode3/data_processing/peptide_match.py
+++ src/CircCode3/data_processing/peptide_match.py
@@ -44,7 +44,7 @@ def mapping_to_junction(junction, remove_peptide, peptide_file):
         dict_junction[Seq.id] = Seq.seq
     table = {}
     with open(remove_peptide) as fi:
-        for line in fi.readlines():
+        for line in fi.readlines()[1:]:
             li = line.strip().split("\t")
             peptide = li[0]
             if peptide.startswith("r"):
